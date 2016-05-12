93c31a977ed03ab820e7e0859d31bdbefb1ea40b
Hruska <eugen.hruska@rice.edu>
2016-05-12 10:09:10 -0500

    pip enabled

--- setup.py
+++ setup.py
@@ -5,8 +5,10 @@ from distutils.core import setup
 setup(name='densplot',
       version='1.0',
       packages=['densplot'],
+      description=['Plotting free energy landscapes'],
       scripts = ['bin/densplot'],
       license='LICENSE.txt',
+      url='https://github.com/ClementiGroup/DensityPlotMaker',
       description='DensPlot package',
-      long_description=open('README.txt').read(),
+      long_description=open('README.md').read(),
      )
